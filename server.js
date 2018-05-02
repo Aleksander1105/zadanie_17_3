e80498cd6db52331dc84a81e2f03e9ebdb82b1fd
Aleksander Matyjewicz <matyjewiczaleksander@gmail.com>
2018-05-02 15:29:43 +0200

    Zadanie_17_4-DONE!

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-
 const app = express();
-let stringifyFile;
 
-app.use(bodyParser.json());
-
-app.get('/getNote', function(req, res) {
-	fs.readFile('./test.json', 'utf-8', function(err, data) {
-		if (err) throw err;
-		stringifyFile = data
-		res.send(data);
-	});
+app.get('/userform', function(req, res) {
+	const response = {
+		first_name: req.query.first_name,
+		last_name: req.query.last_name
+	};
+	res.end(JSON.stringify(response));
+	res.sendFile(__dirname + '/assets/index.html');
 });
 
-app.post('/updateNote/:note', function(req, res) {
-	fs.writeFile('./test.json', stringifyFile, function(err) {
-		if(err) throw err;
-		console.log('file updated!');
-		res.send(req.params.note + stringifyFile);
-	});
-	
-});
+app.use(express.static('assets'));
 
-app.listen(3000);
+const server = app.listen(3000, 'localhost', function() {
+	const host = server.address().address;
+	const port = server.address().port;
+
+	console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
